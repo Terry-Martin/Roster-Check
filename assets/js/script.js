33f3c0f0b0fe1e2c3ccbef1478df6fa35748b45b
Terry Martin <terry.martin@gmail.com>
2024-06-21 16:15:47 +0000

    Add test calc to js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+
+//parseInt(document.getElementById("shift-one").textContent);
+
+let x = parseInt(document.getElementById("shift-one").value);
+alert(x);
+document.getElementById("calc-shift-one").value = 25 + x;
+
