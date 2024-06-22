c7be8e754c376abae875842a3e6c88a146a84f0b
Terry Martin <terry.martin@gmail.com>
2024-06-22 13:17:48 +0000

    Test js to set number of table rows based on dropdown selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
-
-
 //parseInt(document.getElementById("shift-one").textContent);
 
 let x = parseInt(document.getElementById("shift-one").value);
-alert(x);
 document.getElementById("calc-shift-one").value = 25 + x;
 
+let noOfWeeks = document.getElementById("weeks").value;
+alert(noOfWeeks);
+
+let test3 = document.getElementById("week-number").innerHTML;
+
+for (let i = 0; i < noOfWeeks; i++) {
+  test3 += "<tr><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td></tr>" + i;
+}
+
+document.getElementById("week-number").innerHTML = test3;
