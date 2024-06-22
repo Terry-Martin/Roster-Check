e51de4f61c77d32ca0a0281572711a9bfb9859e6
Terry Martin <terry.martin@gmail.com>
2024-06-22 19:04:15 +0000

    Test js delete row and insert cell properies for table

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,7 @@ function myFunction() {
   let inputTableRows = document.getElementById("input-table").innerHTML;
   let calcTableRows = document.getElementById("calc-table").innerHTML;
 
+
   inputTableRows = "<tr><th>Sunday</th><th>Monday</th><th>Tuesday</th><th>Wednesday</th><th>Thursday</th><th>Friday</th><th>Saturday</th></tr><tr><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td> <td><input type='text'></td><td><input type='text'></td></tr>";
   calcTableRows = " <tr><th>Sunday</th><th>Monday</th><th>Tuesday</th><th>Wednesday</th><th>Thursday</th><th>Friday</th><th>Saturday</th></tr><tr><td><input type='text'readonly></td><td><input type='text' readonly></td><td><input type='text' readonly></td><td><input type='text' readonly></td><td><input type='text' readonly></td> <td><input type='text' readonly></td><td><input type='text' readonly></td></tr>";
 
@@ -23,5 +24,37 @@ function myFunction() {
 
   document.getElementById("input-table").innerHTML = inputTableRows;
   document.getElementById("calc-table").innerHTML = calcTableRows;
+
+
+
+  
+
+
+  let table = document.getElementById("test-table");
+
+  for (var i = 1; i < table.rows.length;) {
+    table.deleteRow(i);
+  }
+
+  for (let i = 0; i < noOfWeeks; i++) {
+
+    let row = table.insertRow(-1);
+
+    let c1 = row.insertCell(0);
+    let c2 = row.insertCell(1);
+    let c3 = row.insertCell(2);
+    let c4 = row.insertCell(3);
+    let c5 = row.insertCell(4);
+    let c6 = row.insertCell(5);
+    let c7 = row.insertCell(6);
+
+    c1.innerText = "0800";
+    c2.innerText = "0900";
+    c3.innerText = "0900";
+    c4.innerText = "0900";
+    c5.innerText = "0900";
+    c6.innerText = "0900";
+    c7.innerText = "0900";
+  }
 
 }
