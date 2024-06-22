b7ef9f49378c56fceca244ee714843ba15381fd4
Terry Martin <terry.martin@gmail.com>
2024-06-22 14:19:27 +0000

    Test js event listener for dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,23 @@
 //parseInt(document.getElementById("shift-one").textContent);
+//let x = parseInt(document.getElementById("shift-one").value);
+//document.getElementById("calc-shift-one").value = 25 + x;
 
-let x = parseInt(document.getElementById("shift-one").value);
-document.getElementById("calc-shift-one").value = 25 + x;
+let xy = document.getElementById("weeks");
+xy.addEventListener("change", myFunction);
 
-let noOfWeeks = document.getElementById("weeks").value;
-alert(noOfWeeks);
+function myFunction() {
+  let noOfWeeks = document.getElementById("weeks").value;
+  let inputTableRows = document.getElementById("input-table").innerHTML;
+  let calcTableRows = document.getElementById("calc-table").innerHTML;
 
-let test3 = document.getElementById("week-number").innerHTML;
+  for (let i = 1; i < noOfWeeks; i++) {
+    inputTableRows += "<tr><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td></tr>" + i;
+  }
 
-for (let i = 0; i < noOfWeeks; i++) {
-  test3 += "<tr><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td></tr>" + i;
+  for (let i = 1; i < noOfWeeks; i++) {
+    calcTableRows += "<tr><td><input type='text' readonly></td><td><input type='text' readonly></td><td><input type='text' readonly></td><td><input type='text' readonly></td><td><input type='text' readonly></td><td><input type='text' readonly></td><td><input type='text' readonly></td></tr>" + i;
+  }
+
+  document.getElementById("input-table").innerHTML = inputTableRows;
+  document.getElementById("calc-table").innerHTML = calcTableRows;
 }
-
-document.getElementById("week-number").innerHTML = test3;
