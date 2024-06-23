a27354ba494969105a5db785cfb687be7542c7e4
Terry Martin <terry.martin@gmail.com>
2024-06-23 10:50:38 +0000

    Edit js so less html in js file. Change input type to time

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,40 +5,45 @@
 let xy = document.getElementById("calculate-roster");
 xy.addEventListener("click", myFunction);
 
+
 function myFunction() {
   let noOfWeeks = document.getElementById("weeks").value;
-  let inputTableRows = document.getElementById("input-table").innerHTML;
-  let calcTableRows = document.getElementById("calc-table").innerHTML;
+  let inputTable = document.getElementById("input-table");
+  let calcTable = document.getElementById("calc-table");
 
-
-  inputTableRows = "<tr><th>Sunday</th><th>Monday</th><th>Tuesday</th><th>Wednesday</th><th>Thursday</th><th>Friday</th><th>Saturday</th></tr><tr><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td> <td><input type='text'></td><td><input type='text'></td></tr>";
-  calcTableRows = " <tr><th>Sunday</th><th>Monday</th><th>Tuesday</th><th>Wednesday</th><th>Thursday</th><th>Friday</th><th>Saturday</th></tr><tr><td><input type='text'readonly></td><td><input type='text' readonly></td><td><input type='text' readonly></td><td><input type='text' readonly></td><td><input type='text' readonly></td> <td><input type='text' readonly></td><td><input type='text' readonly></td></tr>";
-
-  for (let i = 1; i < noOfWeeks; i++) {
-    inputTableRows += "<tr><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td><td><input type='text'></td></tr>";
-  }
-
-  for (let i = 1; i < noOfWeeks; i++) {
-    calcTableRows += "<tr><td><input type='text' readonly></td><td><input type='text' readonly></td><td><input type='text' readonly></td><td><input type='text' readonly></td><td><input type='text' readonly></td><td><input type='text' readonly></td><td><input type='text' readonly></td></tr>";
-  }
-
-  document.getElementById("input-table").innerHTML = inputTableRows;
-  document.getElementById("calc-table").innerHTML = calcTableRows;
-
-
-
-  
-
-
-  let table = document.getElementById("test-table");
-
-  for (var i = 1; i < table.rows.length;) {
-    table.deleteRow(i);
+  for (let i = 1; i < inputTable.rows.length;) {
+    inputTable.deleteRow(i);
   }
 
   for (let i = 0; i < noOfWeeks; i++) {
 
-    let row = table.insertRow(-1);
+    let row = inputTable.insertRow(-1);
+
+    let c1 = row.insertCell(0);
+    let c2 = row.insertCell(1);
+    let c3 = row.insertCell(2);
+    let c4 = row.insertCell(3);
+    let c5 = row.insertCell(4);
+    let c6 = row.insertCell(5);
+    let c7 = row.insertCell(6);
+
+    c1.innerHTML = "<td></td><input type='time'></td>";
+    c2.innerHTML = "<td></td><input type='time'></td>";
+    c3.innerHTML= "<td></td><input type='time'></td>";
+    c4.innerHTML = "<td></td><input type='time'></td>";
+    c5.innerHTML = "<td></td><input type='time'></td>";
+    c6.innerHTML = "<td></td><input type='time'></td>";
+    c7.innerHTML = "<td></td><input type='time'></td>";
+
+  }
+
+  for (let i = 1; i < calcTable.rows.length;) {
+    calcTable.deleteRow(i);
+  }
+
+  for (let i = 0; i < noOfWeeks; i++) {
+
+    let row = calcTable.insertRow(-1);
 
     let c1 = row.insertCell(0);
     let c2 = row.insertCell(1);
@@ -54,7 +59,7 @@ function myFunction() {
     c4.innerText = "0900";
     c5.innerText = "0900";
     c6.innerText = "0900";
-    c7.innerText = "0900";
-  }
+    c7.innerHTML = "0800";
 
+  }
 }
