4f22a39a04f7d5c1a41441d24aeb6b3defaef020
Terry Martin <terry.martin@gmail.com>
2024-06-28 21:24:25 +0000

    Update populateShiftLengthTable function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 addRows();
+//populateShiftLengthTable();
 
 let rosterLength = document.getElementById("calculate-roster");
-rosterLength.addEventListener("click", addRows);
+rosterLength.addEventListener("click", addRows, populateShiftLengthTable);
 
 let calcShift = document.getElementById("calculate-shift");
 calcShift.addEventListener("click", calcShiftLength);
@@ -43,13 +44,13 @@ function addRows() {
       let c14 = row.insertCell(13);
 
       cellNumber++;
-      c1.innerHTML = `<td><input type='time' value='09:00' id='input${cellNumber}'></td>`;
+      c1.innerHTML = `<td><input type='time' value='00:00' id='input${cellNumber}'></td>`;
       cellNumber++;
-      c2.innerHTML = `<td><input type='time' value='18:00' id='input${cellNumber}'></td>`;
+      c2.innerHTML = `<td><input type='time' value='01:00' id='input${cellNumber}'></td>`;
       cellNumber++;
       c3.innerHTML = `<td><input type='time' value='09:00' id='input${cellNumber}'></td>`;
       cellNumber++;
-      c4.innerHTML = `<td><input type='time' value='18:00' id='input${cellNumber}'></td>`;
+      c4.innerHTML = `<td><input type='time' value='11:00' id='input${cellNumber}'></td>`;
       cellNumber++;
       c5.innerHTML = `<td><input type='time' value='09:00' id='input${cellNumber}'></td>`;
       cellNumber++;
@@ -67,85 +68,66 @@ function addRows() {
       cellNumber++;
       c12.innerHTML = `<td><input type='time' value='18:00' id='input${cellNumber}'></td>`;
       cellNumber++;
-      c13.innerHTML = `<td><input type='time' value='09:00' id='input${cellNumber}'></td>`;
+      c13.innerHTML = `<td><input type='time' value='00:00' id='input${cellNumber}'></td>`;
       cellNumber++;
-      c14.innerHTML = `<td><input type='time' value='18:00' id='input${cellNumber}'></td>`;
+      c14.innerHTML = `<td><input type='time' value='00:00' id='input${cellNumber}'></td>`;
     }
-
+    populateShiftLengthTable();
   }
 }
 
-function populateShiftCalcTable() {
+function populateShiftLengthTable() {
+
+  // Add ID to dynamicaally created cells
+  // https://stackoverflow.com/questions/23003341/add-id-dynamically-to-each-table-cells
+
 
   let noOfWeeks = document.getElementById("weeks").value;
-  let calcTable = document.getElementById("calc-table");
+  let displayTable = document.getElementById("display-shift-length-table");
 
-  // Check and change var to let
-  var curCellIndex = 0;
+  for (let i = 2; i < displayTable.rows.length;) {
+    displayTable.deleteRow(i);
+  }
 
-  if (noOfWeeks < 1) {
-    alert("Rotation needs to be a minimum of 1 week")
+  let cellNumber = 0;
 
-  } else {
+  for (let i = 0; i < noOfWeeks; i++) {
 
-    for (let i = 1; i < calcTable.rows.length;) {
-      calcTable.deleteRow(i);
-    }
+    let row = displayTable.insertRow(-1);
 
-    for (let i = 0; i < noOfWeeks; i++) {
+    let c1 = row.insertCell(0);
+    let c2 = row.insertCell(1);
+    let c3 = row.insertCell(2);
+    let c4 = row.insertCell(3);
+    let c5 = row.insertCell(4);
+    let c6 = row.insertCell(5);
+    let c7 = row.insertCell(6);
 
-      // Add ID to dynamicaally created cells
-      // https://stackoverflow.com/questions/23003341/add-id-dynamically-to-each-table-cells
+    cellNumber++;
+    c1.innerHTML = `<td><input type='text' id='display${cellNumber}' readonly></td>`;
+    cellNumber++;
+    c2.innerHTML = `<td><input type='text' id='display${cellNumber}' readonly></td>`;
+    cellNumber++;
+    c3.innerHTML = `<td><input type='text' id='display${cellNumber}' readonly></td>`;
+    cellNumber++;
+    c4.innerHTML = `<td><input type='text' id='display${cellNumber}' readonly></td>`;
+    cellNumber++;
+    c5.innerHTML = `<td><input type='text' id='display${cellNumber}' readonly></td>`;
+    cellNumber++;
+    c6.innerHTML = `<td><input type='text' id='display${cellNumber}' readonly></td>`;
+    cellNumber++;
+    c7.innerHTML = `<td><input type='text' id='display${cellNumber}' readonly></td>`;
 
-
-      let row = calcTable.insertRow(-1);
-
-      let c1 = row.insertCell(0);
-      c1.id = "cell-" + curCellIndex;
-      curCellIndex++;
-
-      let c2 = row.insertCell(1);
-      c2.id = "cell-" + curCellIndex;
-      curCellIndex++;
-
-      let c3 = row.insertCell(2);
-      c3.id = "cell-" + curCellIndex;
-      curCellIndex++;
-
-      let c4 = row.insertCell(3);
-      c4.id = "cell-" + curCellIndex;
-      curCellIndex++;
-
-      let c5 = row.insertCell(4);
-      c5.id = "cell-" + curCellIndex;
-      curCellIndex++;
-
-      let c6 = row.insertCell(5);
-      c6.id = "cell-" + curCellIndex;
-      curCellIndex++;
-
-      let c7 = row.insertCell(6);
-      c7.id = "cell-" + curCellIndex;
-      curCellIndex++;
-
-      c1.innerText = 1;
-      c2.innerText = 2;
-      c3.innerText = 3;
-      c4.innerText = 4;
-      c5.innerText = 5;
-      c6.innerText = 6;
-      c7.innerText = 7;
-
-    }
   }
 }
 
 function calcShiftLength() {
 
-  let myTable = document.getElementById("input-table");
+  let inputTable = document.getElementById("input-table");
 
   // Get the number of shift cells in the table
-  let shiftsInTable = ((myTable.rows.length) - 2) * 14;
+  let shiftsInTable = ((inputTable.rows.length) - 2) * 14;
+  let displayTableCells = 1;
 
   for (let cellNumber = 1; cellNumber < shiftsInTable;) {
 
@@ -163,11 +145,9 @@ function calcShiftLength() {
     let shiftLength = (endMinutes - startMinutes) / 60;
 
     shiftLength = shiftLength.toFixed(2);
-    if (shiftLength < 0) {
-      alert("Shift end time needs to be after shift start time");
-    } else {
-      alert(shiftLength);
+ 
+    document.getElementById(`display${displayTableCells}`).value = shiftLength;
+    displayTableCells++;
 
-    }
   }
 }
